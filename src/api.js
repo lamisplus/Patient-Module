6be9b22a0df4aec971ed1c8bf861cf876dd937e6
Basil-DataFI <basil.ohuruogu@thepalladiumgroup.com>
2025-07-25 11:42:46 +0100

    DFN-2760:[FIX] Lab test orders grid keeps loading, blocking client verification

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzUyOTQxMzAyfQ.xUJEnYS4F9a558OfcJuGfzmyoUoN1PKQpHlKKxTJhXOazeUGHwQ_8itTqDlhnxptvFKpLVNAZplWp3IXJCrzAA"
+    ? ""
     : new URLSearchParams(window.location.search).get("jwt");
 
 export const wsUrl =
